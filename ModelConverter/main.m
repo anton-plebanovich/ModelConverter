d718a80e5724a11541b6ffca6cc66c25e7754308
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2016-02-10 11:54:14 +0300

    Initial Commit

---
+++ ModelConverter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ModelConverter
+//
+//  Created by mac-246 on 10.02.16.
+//  Copyright © 2016 mac-246. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
